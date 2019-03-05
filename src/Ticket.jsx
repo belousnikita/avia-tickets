3d8d5c8f3b38665dc31edca8f0a61f49157ca525
Nikita Belous <belousnikita@gmail.com>
2019-03-05 19:46:26 +0200

    Added ticket rendering

--- src/Ticket.jsx
+++ src/Ticket.jsx
@@ -13,16 +13,25 @@ import Button from './Button';
 //     "stops": 3,
 //     "price": 12400
 
-const TimeBlock = props => (<div >
-    <div>19:50</div>
-    <div>KBP, Kyiv</div>
-    <div>19 nov, 2019 fr</div>
+const TimeBlock = props => (<div className="stamp" >
+    <div className="time">{props.time}</div>
+    <div className="destination">{props.ap}, {props.ap_name}</div>
+    <div className="date">{props.date}</div>
 </div>);
-const Transfer = props => (<div className="transfer d-flex align-items-center">1 Transfer</div>);
+const Transfer = props => (<div className="col transfer align-items-center">
+    <div className="text">{props.stops === 0 ? 'Direct'
+        : props.stops > 1 ? `${props.stops} transfers` : '1 transfer'}</div>
+    <div className="line-wrapper">
+        <div className="line"></div>
+        <i className="icon fas fa-plane"></i>
+    </div>
+</div>);
 export default class Ticket extends React.Component {
     render() {
+
         const { origin,
             origin_name,
+            destination,
             destination_name,
             departure_date,
             departure_time,
@@ -31,19 +40,31 @@ export default class Ticket extends React.Component {
             carrier,
             stops,
             price } = this.props;
-
-        return <div className="block row">
-            <div class="col-auto">
-                <div>Carrier</div>
-                <Button className="btn large" name="buy" />
-            </div>
-            <div className="d-flex flex-column">
+        return <div className="block ticket row align-self-center">
+            <div className="col-auto d-flex flex-column ml-auto p-2">
                 <div className="d-flex justify-content-between">
-                    <TimeBlock />
-                    <Transfer />
-                    <TimeBlock />
+                    <TimeBlock time={departure_time} ap={origin} ap_name={origin_name} date={departure_date} />
+                    <Transfer stops={stops} />
+                    <TimeBlock time={arrival_time} ap={destination} ap_name={destination_name} date={arrival_date} />
                 </div>
+            </div>
+            <div className="col-auto align-self-center" >
+                <div className="text">{carrier}</div>
+                <Button className="btn buy" name={`${price}`} />
             </div>
         </div>
     }
+}
+Ticket.propTypes = {
+    origin: PropTypes.string,
+    origin_name: PropTypes.string,
+    destination: PropTypes.string,
+    destination_name: PropTypes.string,
+    departure_date: PropTypes.string,
+    departure_time: PropTypes.string,
+    arrival_date: PropTypes.string,
+    arrival_time: PropTypes.string,
+    carrier: PropTypes.string,
+    stops: PropTypes.number,
+    price: PropTypes.string,
 }
