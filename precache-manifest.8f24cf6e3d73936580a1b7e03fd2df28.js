59e33c0084aa61f0374596914668912ff233e0b0
Nikita Belous <belousnikita@gmail.com>
2019-03-05 20:20:38 +0200

    Updates

---
+++ precache-manifest.8f24cf6e3d73936580a1b7e03fd2df28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5481fbb5e785ed4a5fd",
+    "url": "/chern0us/avia-tickets/static/js/runtime~main.a5481fbb.js"
+  },
+  {
+    "revision": "d051eab3026f4eaaefb1",
+    "url": "/chern0us/avia-tickets/static/js/main.d051eab3.chunk.js"
+  },
+  {
+    "revision": "f0b46cd4e7a372ecf1d0",
+    "url": "/chern0us/avia-tickets/static/js/2.f0b46cd4.chunk.js"
+  },
+  {
+    "revision": "d051eab3026f4eaaefb1",
+    "url": "/chern0us/avia-tickets/static/css/main.25f1d170.chunk.css"
+  },
+  {
+    "revision": "06a2da87f5a02f3b3a0d9763ec50f960",
+    "url": "/chern0us/avia-tickets/index.html"
+  }
+];
